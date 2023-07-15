d443dcc8ca62f43cd2b46b7ff4cccd6784930547
Matt <matiaslucero21884@gmail.com>
2023-07-15 10:52:59 -0300

    Correccion Heart

--- src/Components/ColorCard/ColorCard.jsx
+++ src/Components/ColorCard/ColorCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./ColorCard.css"
+import "../NavBar/NavBar.css"
 
 const ColorCard = ({hours,likes,colors}) => {
     return (
         <div className='colorCardContainer' >   
             <div className='colorCard'>
-                <div style={{ backgroundColor: "#" + colors[0] }}></div>
-                <div style={{ backgroundColor: "#" + colors[1] }}></div>
-                <div style={{ backgroundColor: "#" + colors[2] }}></div>
-                <div style={{ backgroundColor: "#" + colors[3] }}></div>
+                <div style={{ backgroundColor: "#" + colors[0] }}>{"."}</div>
+                <div style={{ backgroundColor: "#" + colors[1] }}>{"."}</div>
+                <div style={{ backgroundColor: "#" + colors[2] }}>{"."}</div>
+                <div style={{ backgroundColor: "#" + colors[3] }}>{"."}</div>
             </div>"#" + 
             <div className='colorCardInfo'>
                 <button>
